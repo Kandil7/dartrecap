c49322caa4a17407e38b6830ce3dff68a6d3658f
Mohamed Kandil <105888370+Kandil7@users.noreply.github.com>
2025-02-21 09:20:59 -0800

    cascade in classes

---
+++ classes/test_class.dart
@@ -0,0 +1,10 @@
+import 'dart:math';
+
+class Test{
+  String val1="one";
+  String val2="two";
+  int randomValue(){
+    print('Random!');
+    return Random().nextInt(10);
+  }
+}
--- main.dart
+++ main.dart
@@ -1,8 +1,34 @@
 
 import 'classes/person.dart';
+import 'classes/test_class.dart';
 
 void main() {
 var customer=Person("Mohamed", "Kandil");
 print(customer.name);
 print(customer.surname);
+
+Test test=Test();
+
+print(test.val1);
+print(test.val2);
+
+// pass a value to variable with a classic way
+test.val1="one_1";
+test.val2="two_2";
+print(test.val1);
+print(test.val2);
+// pass a value to variable in 'cascade '
+test..val1='one_1_cascade'
+..val2='two_2_cascade';
+
+print(test.val1);
+print(test.val2);
+
+// implement class method in cascade way
+
+test..randomValue()
+..randomValue()
+..randomValue();
+print(test.randomValue());
+
 }
